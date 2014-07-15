12bda0225bb4f1d1dfc44ba296ed351229ad71e7
DoriPS13 <palmer.sanderson@gmail.com>
2014-07-14 20:07:58 -0700

    woo I think this is right

---
+++ todo.js
@@ -0,0 +1,27 @@
+var form = document.getElementById('to_do_form');
+var input = document.getElementById('to_do_input');
+var list = document.getElementById('list');
+var remove = document.getElementsByTagName('button')
+
+
+form.onsubmit = function () {
+	event.preventDefault();
+
+var ulList = document.createElement('ul');
+var newTextNode = document.createTextNode(input.value);
+var rmButton = document.createElement('button') 
+
+	rmButton.innerHTML = 'X'
+	list.appendChild(ulList)
+	list.appendChild(newTextNode)
+	list.appendChild(rmButton)
+
+	rmButton.onclick = function () {
+
+		rmButton.remove()
+		newTextNode.remove()
+
+	}
+}
+
+
